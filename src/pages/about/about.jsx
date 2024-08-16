3b316d49cb72a0508595d945ba4a88e840098d98
mastervr99 <web.dev418@gmail.com>
2024-08-16 18:35:59 +0200

    feat : page about

--- src/pages/about/about.jsx
+++ src/pages/about/about.jsx
@@ -2,20 +2,19 @@
 import Banner from '../../components/banner/banner';
 import './about.scss';
 import backgroundImage from '../../assets/images/background_about.png';
+import Collapse from '../../components/collapse/collapse';
 
 
 function About() {
     return <div className='about'>
                <Banner backgroundImage={backgroundImage} />
-                <main>
-                    <div>
-                        <ul>
-                            <li>Fiabilité</li>
-                            <li>Respect</li>
-                            <li>Service</li>
-                            <li>Sécurité</li>
-                        </ul>
-                    </div>
+                <main className='collapses'>
+                    <ul className='collapse_list'>
+                        <li><Collapse text="Fiabilité"/></li>
+                        <li><Collapse text="Respect"/></li>
+                        <li><Collapse text="Service"/></li>
+                        <li><Collapse text="Sécurité"/></li>
+                    </ul>
                 </main>
            </div>
 }
