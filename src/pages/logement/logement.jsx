2ec043a13cc96a2c97b74bfad50dd40c405b43a7
mastervr99 <web.dev418@gmail.com>
2024-08-22 20:13:09 +0200

    feat : page home

--- src/pages/logement/logement.jsx
+++ src/pages/logement/logement.jsx
@@ -1,6 +1,11 @@
 import './logement.scss';
 import { useParams } from 'react-router-dom';
 import logements from '../../assets/data/logements.json';
+import Carrousel from '../../components/carrousel/carrousel';
+import Host from '../../components/host/host';
+import Tag from '../../components/tag/tag';
+import Rating from '../../components/rating/rating';
+import Collapse from '../../components/collapse/collapse';
 
 function Logement() {
     const { id } = useParams();
@@ -12,9 +17,28 @@ function Logement() {
 
     return (
         <div className='logement'>
-            <h1>{logement.title}</h1>
-            <img src={logement.cover} alt={logement.title} />
-            <p>{logement.description}</p>
+            <Carrousel pictures={logement.pictures}/>
+            <div className='logement_infos'>
+                <div className='logement_details'>
+                    <h1>{logement.title}</h1>
+                    <p>{logement.location}</p>
+                </div>
+                <Host host={logement.host} />
+            </div>
+            <div className='logement_tags_notation'>
+                <div className='tags_list'>
+                    {logement.tags.map((tag) => (
+                            <Tag tag={tag} />
+                        ))}
+                </div>
+                <Rating rating={logement.rating}/>
+            </div>
+            <div>
+                <ul className='collapse_list'>
+                    <li><Collapse title="Description" text={logement.description}/></li>
+                    <li><Collapse title="Equipements" text={logement.equipments}/></li>
+                </ul>
+            </div>
         </div>
     );
 }
