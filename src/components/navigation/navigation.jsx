6ffe401f920c798e61c9ce875ba37b049ba8652e
mastervr99 <web.dev418@gmail.com>
2024-08-22 20:13:38 +0200

    refactoring : component navigation

--- src/components/navigation/navigation.jsx
+++ src/components/navigation/navigation.jsx
@@ -5,10 +5,10 @@ function Navigation() {
     return <div className='navbar'>
                 <ul className='navList'>
                     <NavLink to="/" end className={({ isActive }) => (isActive ? "nav-active" : "")}>
-                        <li>ACCUEIL</li>
+                        <li>Accueil</li>
                     </NavLink>
                     <NavLink to="/about" end className={({ isActive }) => (isActive ? "nav-active" : "")}>
-                        <li>A PROPOS</li>
+                        <li>A Propos</li>
                     </NavLink>
                 </ul>
             </div>
