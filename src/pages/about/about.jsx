65688d8a81b904b84cf1a9fd7b6c1cbfadcd5ed7
mastervr99 <web.dev418@gmail.com>
2024-08-22 20:14:02 +0200

    feat : page about

--- src/pages/about/about.jsx
+++ src/pages/about/about.jsx
@@ -6,14 +6,23 @@ import Collapse from '../../components/collapse/collapse';
 
 
 function About() {
+
+    const fiabily_text = "Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont confirmes aux logements, et toutes les informations sont régulièrement vérifiées par nos équipes.";
+
+    const respect_text = "La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de pertubation du voisinage entraînera une exlusion de notre plateforme.";
+
+    const service_text = "La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de pertubation du voisinage entraînera une exlusion de notre plateforme.";
+    
+    const security_text = "La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une notre aussi bieen à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes";
+
     return <div className='about'>
                <Banner backgroundImage={backgroundImage} />
                 <main className='collapses'>
                     <ul className='collapse_list'>
-                        <li><Collapse text="Fiabilité"/></li>
-                        <li><Collapse text="Respect"/></li>
-                        <li><Collapse text="Service"/></li>
-                        <li><Collapse text="Sécurité"/></li>
+                        <li><Collapse title="Fiabilité" text={ fiabily_text}/></li>
+                        <li><Collapse title="Respect" text={respect_text}/></li>
+                        <li><Collapse title="Service" text={service_text}/></li>
+                        <li><Collapse title="Sécurité" text={security_text}/></li>
                     </ul>
                 </main>
            </div>
