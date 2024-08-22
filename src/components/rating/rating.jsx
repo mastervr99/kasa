ad857b738ff0b4464e0ce352eae73130ba38fc3f
mastervr99 <web.dev418@gmail.com>
2024-08-22 20:11:59 +0200

    feat : component rating

---
+++ src/components/rating/rating.jsx
@@ -0,0 +1,22 @@
+import './rating.scss';
+
+function Rating({ rating }) {
+    const totalStars = 5;
+    const stars = [];
+
+    for (let i = 0; i < totalStars; i++) {
+        if (i < rating) {
+            stars.push(<span key={i} className='red_star'><i class="fa-solid fa-star"></i></span>);
+        } else {
+            stars.push(<span key={i} className='grey_star'><i class="fa-solid fa-star"></i></span>);
+        }
+    }
+
+    return (
+        <div className='rating'>
+            {stars}
+        </div>
+    );
+}
+
+export default Rating;
