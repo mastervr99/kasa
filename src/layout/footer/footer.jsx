6e2bbfbfc406aea08f398e493b2d9517f8400f8a
mastervr99 <web.dev418@gmail.com>
2024-08-28 01:32:31 +0200

    fix : footer position on page about

--- src/layout/footer/footer.jsx
+++ src/layout/footer/footer.jsx
@@ -1,15 +1,23 @@
 import './footer.scss';
 import LOGO_footer from '../../assets/images/LOGO_footer.png';
+import { useLocation } from 'react-router-dom';
 
 function Footer() {
-    return <footer className='footer'>
-                <div>
-                    <img src={LOGO_footer}/>
-                </div>
-                <div className='copyright'>
-                    <p>© 2020 Kasa. All rights reserved</p>
-                </div>
-            </footer>
+
+    const location = useLocation();
+
+    const isAboutPage = location.pathname === '/about';
+
+    return (
+        <footer className={`footer ${isAboutPage ? 'footer-absolute' : ''}`}>
+            <div>
+                <img src={LOGO_footer} alt="Logo Kasa" />
+            </div>
+            <div className='copyright'>
+                <p>© 2020 Kasa. All rights reserved</p>
+            </div>
+        </footer>
+    );
 }
 
 export default Footer
